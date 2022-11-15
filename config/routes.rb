553c9dbcd307015462104cf26dfe4073126a2412
Renoir Lucena <renoirlucena@gmail.com>
2022-11-15 16:27:56 -0300

    version with the edit link

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: :task
 
   # UPDATE a task
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
 
   # DELETE a restaurant
